fbcaaeed66a93674a4afd52bec055054ce527f1c
gettoknowdavid <knowdavidmichael@gmail.com>
2025-03-18 00:37:47 +0100

    add assets and fonts with `flutter_gen`

---
+++ lib/src/assets/assets.gen.dart
@@ -0,0 +1,387 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
+
+class $AssetsEmojisGen {
+  const $AssetsEmojisGen();
+
+  /// File path: assets/emojis/celebrate.png
+  AssetGenImage get celebrate =>
+      const AssetGenImage('assets/emojis/celebrate.png');
+
+  /// File path: assets/emojis/clapping hands.png
+  AssetGenImage get clappingHands =>
+      const AssetGenImage('assets/emojis/clapping hands.png');
+
+  /// File path: assets/emojis/collision.png
+  AssetGenImage get collision =>
+      const AssetGenImage('assets/emojis/collision.png');
+
+  /// File path: assets/emojis/finger snap.png
+  AssetGenImage get fingerSnap =>
+      const AssetGenImage('assets/emojis/finger snap.png');
+
+  /// File path: assets/emojis/flame.png
+  AssetGenImage get flame => const AssetGenImage('assets/emojis/flame.png');
+
+  /// File path: assets/emojis/high voltage.png
+  AssetGenImage get highVoltage =>
+      const AssetGenImage('assets/emojis/high voltage.png');
+
+  /// File path: assets/emojis/raising hands.png
+  AssetGenImage get raisingHands =>
+      const AssetGenImage('assets/emojis/raising hands.png');
+
+  /// File path: assets/emojis/red heart.png
+  AssetGenImage get redHeart =>
+      const AssetGenImage('assets/emojis/red heart.png');
+
+  /// File path: assets/emojis/sparkles.png
+  AssetGenImage get sparkles =>
+      const AssetGenImage('assets/emojis/sparkles.png');
+
+  /// File path: assets/emojis/thumbs up.png
+  AssetGenImage get thumbsUp =>
+      const AssetGenImage('assets/emojis/thumbs up.png');
+
+  /// File path: assets/emojis/waving hand.png
+  AssetGenImage get wavingHand =>
+      const AssetGenImage('assets/emojis/waving hand.png');
+
+  /// File path: assets/emojis/writing hand.png
+  AssetGenImage get writingHand =>
+      const AssetGenImage('assets/emojis/writing hand.png');
+
+  /// Directory path: packages/meno_design_system/assets/emojis
+  String get path => 'packages/meno_design_system/assets/emojis';
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    celebrate,
+    clappingHands,
+    collision,
+    fingerSnap,
+    flame,
+    highVoltage,
+    raisingHands,
+    redHeart,
+    sparkles,
+    thumbsUp,
+    wavingHand,
+    writingHand,
+  ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// Directory path: assets/images/brand_logos
+  $AssetsImagesBrandLogosGen get brandLogos =>
+      const $AssetsImagesBrandLogosGen();
+
+  /// File path: assets/images/folder.png
+  AssetGenImage get folder => const AssetGenImage('assets/images/folder.png');
+
+  /// File path: assets/images/geometric lines.svg
+  SvgGenImage get geometricLines =>
+      const SvgGenImage('assets/images/geometric lines.svg');
+
+  /// File path: assets/images/ic_launcher.png
+  AssetGenImage get icLauncher =>
+      const AssetGenImage('assets/images/ic_launcher.png');
+
+  /// File path: assets/images/live_for_you.png
+  AssetGenImage get liveForYou =>
+      const AssetGenImage('assets/images/live_for_you.png');
+
+  /// File path: assets/images/loading.gif
+  AssetGenImage get loading => const AssetGenImage('assets/images/loading.gif');
+
+  /// Directory path: assets/images/logo
+  $AssetsImagesLogoGen get logo => const $AssetsImagesLogoGen();
+
+  /// File path: assets/images/new-file.png
+  AssetGenImage get newFile =>
+      const AssetGenImage('assets/images/new-file.png');
+
+  /// Directory path: assets/images/onboarding
+  $AssetsImagesOnboardingGen get onboarding =>
+      const $AssetsImagesOnboardingGen();
+
+  /// File path: assets/images/success.png
+  AssetGenImage get success => const AssetGenImage('assets/images/success.png');
+
+  /// Directory path: packages/meno_design_system/assets/images
+  String get path => 'packages/meno_design_system/assets/images';
+
+  /// List of all assets
+  List<dynamic> get values => [
+    folder,
+    geometricLines,
+    icLauncher,
+    liveForYou,
+    loading,
+    newFile,
+    success,
+  ];
+}
+
+class $AssetsImagesBrandLogosGen {
+  const $AssetsImagesBrandLogosGen();
+
+  /// File path: assets/images/brand_logos/facebook.svg
+  SvgGenImage get facebook =>
+      const SvgGenImage('assets/images/brand_logos/facebook.svg');
+
+  /// File path: assets/images/brand_logos/google.svg
+  SvgGenImage get google =>
+      const SvgGenImage('assets/images/brand_logos/google.svg');
+
+  /// Directory path: packages/meno_design_system/assets/images/brand_logos
+  String get path => 'packages/meno_design_system/assets/images/brand_logos';
+
+  /// List of all assets
+  List<SvgGenImage> get values => [facebook, google];
+}
+
+class $AssetsImagesLogoGen {
+  const $AssetsImagesLogoGen();
+
+  /// File path: assets/images/logo/logo-dark.svg
+  SvgGenImage get logoDark =>
+      const SvgGenImage('assets/images/logo/logo-dark.svg');
+
+  /// File path: assets/images/logo/logo-light.svg
+  SvgGenImage get logoLight =>
+      const SvgGenImage('assets/images/logo/logo-light.svg');
+
+  /// File path: assets/images/logo/logo.png
+  AssetGenImage get logo => const AssetGenImage('assets/images/logo/logo.png');
+
+  /// File path: assets/images/logo/meno-purple.png
+  AssetGenImage get menoPurple =>
+      const AssetGenImage('assets/images/logo/meno-purple.png');
+
+  /// File path: assets/images/logo/meno-white.png
+  AssetGenImage get menoWhite =>
+      const AssetGenImage('assets/images/logo/meno-white.png');
+
+  /// File path: assets/images/logo/splash-logo.png
+  AssetGenImage get splashLogo =>
+      const AssetGenImage('assets/images/logo/splash-logo.png');
+
+  /// Directory path: packages/meno_design_system/assets/images/logo
+  String get path => 'packages/meno_design_system/assets/images/logo';
+
+  /// List of all assets
+  List<dynamic> get values => [
+    logoDark,
+    logoLight,
+    logo,
+    menoPurple,
+    menoWhite,
+    splashLogo,
+  ];
+}
+
+class $AssetsImagesOnboardingGen {
+  const $AssetsImagesOnboardingGen();
+
+  /// File path: assets/images/onboarding/onboarding-1.png
+  AssetGenImage get onboarding1 =>
+      const AssetGenImage('assets/images/onboarding/onboarding-1.png');
+
+  /// File path: assets/images/onboarding/onboarding-2.png
+  AssetGenImage get onboarding2 =>
+      const AssetGenImage('assets/images/onboarding/onboarding-2.png');
+
+  /// File path: assets/images/onboarding/onboarding-3.png
+  AssetGenImage get onboarding3 =>
+      const AssetGenImage('assets/images/onboarding/onboarding-3.png');
+
+  /// File path: assets/images/onboarding/onboarding-4.png
+  AssetGenImage get onboarding4 =>
+      const AssetGenImage('assets/images/onboarding/onboarding-4.png');
+
+  /// Directory path: packages/meno_design_system/assets/images/onboarding
+  String get path => 'packages/meno_design_system/assets/images/onboarding';
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    onboarding1,
+    onboarding2,
+    onboarding3,
+    onboarding4,
+  ];
+}
+
+class MenoAssets {
+  const MenoAssets._();
+
+  static const String package = 'meno_design_system';
+
+  static const $AssetsEmojisGen emojis = $AssetsEmojisGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}});
+
+  final String _assetName;
+
+  static const String package = 'meno_design_system';
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+  }) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/meno_design_system/$_assetName';
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = false;
+
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  static const String package = 'meno_design_system';
+
+  _svg.SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    _svg.SvgTheme? theme,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final _svg.BytesLoader loader;
+    if (_isVecFormat) {
+      loader = _vg.AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = _svg.SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+      );
+    }
+    return _svg.SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter:
+          colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/meno_design_system/$_assetName';
+}
