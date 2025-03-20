c6c5488dc99891127dc48d17bbbf04a1609261b3
gettoknowdavid <credential.helper>
2025-03-20 09:02:39 +0100

    add `MenoTextfield` widget

---
+++ lib/src/components/inputs/inputs.dart
@@ -0,0 +1,4 @@
+export 'meno_input_label.dart';
+export 'meno_textfield.dart';
+export 'meno_validation_text.dart';
+export 'password_eye.dart';
---
+++ lib/src/components/inputs/password_eye.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meno_design_system/meno_design_system.dart';
+import 'package:meno_design_system/src/components/inputs/meno_textfield.dart';
+
+/// {@template password_eye}
+/// A custom widget for enabling and disabling the [MenoTextfield]
+/// `obscureText` flag
+/// {@endtemplate}
+class PasswordEye extends StatelessWidget {
+  /// {@macro password_eye}
+  const PasswordEye({
+    required this.obscureText,
+    super.key,
+    this.size = MenoSize.md,
+  });
+
+  /// Boolean flag to hide or display the input value
+  final bool obscureText;
+
+  /// [MenoSize] value
+  final MenoSize size;
+
+  @override
+  Widget build(BuildContext context) {
+    return MenoInputIcon(
+      obscureText
+          ? const Icon(Icons.visibility)
+          : const Icon(Icons.visibility_off),
+      size: size,
+      alignment: Alignment.centerLeft,
+    );
+  }
+}
